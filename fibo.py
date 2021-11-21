574e60f8d105be4464f22728b0e24c06316fc61f
RomanZmushko <Official.zmushko@yandex.ru>
2021-11-21 13:30:18 +0300

    -- # 3d commit. changes in the order of assigning values to variables in one line

--- fibo.py
+++ fibo.py
@@ -1,10 +1,9 @@
 def fibo (x):
-    a,b,c = 1, 1, 0
+    a, b, c = 1, 1, 0
     for elem in range (x):
         print (a)
         c = a + b
-        a = b
-        b = c
+        a, b = b, c
 
 
 fibo(5)
